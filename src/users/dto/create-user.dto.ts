4d1611aeaddd3ea53a4b58954e3a0f76b298710d
Diplomat761 <l7693744@gmail.com>
2023-03-26 22:33:12 +0700

    добавил валидацию

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,16 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString, Length } from "class-validator";
 
 export class createUserDto {
   @ApiProperty({ example: "user@mail.com", description: "Почтовый адрес" })
+  @IsString({ message: "Только строчные символы" })
+  @IsEmail({}, { message: "Некорректный Email" })
   readonly email: string;
 
+  @IsString({ message: "Только строчные символы" })
+  @Length(4, 16, {
+    message: "Парольдолжны быть не менее 4 и не более 16 символов",
+  })
   @ApiProperty({ example: "root123", description: "Пароль" })
   readonly password: string;
 }
